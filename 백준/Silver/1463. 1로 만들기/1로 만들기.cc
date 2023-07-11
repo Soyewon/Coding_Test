6449c3793b0eb7d494e0ebcd8d64459d3e65927b
Yewon So <86148470+Soyewon@users.noreply.github.com>
2023-07-11 10:51:22 +0900

    Update 1로 만들기.cc

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,28 +1,30 @@
+// 백준 1463 - 1로 만들기
+// https://www.acmicpc.net/problem/1463
+// dp
+
 #include <iostream>
-#include <cstring>
+#include <algorithm>
+
 using namespace std;
 
-int dp[1000001];
+int d[1000001]; // i를 1로 만들 때 사용하는 연산횟수 최솟값 d[i]
+int n;
 
-int min(int a, int b) {
-	if (a > b) return b;
-	else return a;
+void dp(int n) {
+    for(int i=2; i<=n; i++) {
+        d[i] = d[i-1] + 1; // 1을 뺄 때
+        if(i%2 == 0)
+            d[i] = min(d[i], d[i/2]+1);
+        if(i%3 == 0)
+            d[i] = min(d[i], d[i/3]+1);
+    }
 }
 
 int main() {
-	int N;
-	scanf("%d", &N);
+    cin >> n;
+    d[1] = 0;
 
-	for (int i = 2; i <= N; i++) {
-		dp[i] = dp[i - 1] + 1;
-		if (i % 2 == 0) {
-			dp[i] = min(dp[i], dp[i / 2] + 1);
-		}
-		if (i % 3 == 0) {
-			dp[i] = min(dp[i], dp[i / 3] + 1);
-		}
-	}
+    dp(n);
 
-	printf("%d", dp[N]);
-	return 0;
+    cout << d[n];
 }
