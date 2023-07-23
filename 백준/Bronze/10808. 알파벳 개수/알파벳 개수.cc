894ca32ed29c75cab3f10b9afffe1fd11c0151ef
Yewon So <86148470+Soyewon@users.noreply.github.com>
2023-07-23 16:42:00 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int alpha[26];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+
+    for(auto c : s)
+        alpha[c-'a']++;
+    
+    for(int i=0; i<26; i++)
+        cout << alpha[i] << ' ';
+
+    return 0;
+
+}
